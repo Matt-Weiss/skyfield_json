3ed1fb22c24eb9c9962e75b1042bcaaeafc3ddf1
Matt Levy <milevy1@gmail.com>
2019-05-23 21:38:38 -0600

    Adds ephemerides endpoint for dynamic celestial_bodies
    Co-authored-by: Jeremy Bennett <25406708+JaxJafinPapau@users.noreply.github.com>

--- app/skyfield_service.py
+++ app/skyfield_service.py
@@ -13,16 +13,54 @@ class SkyfieldService():
         uranus = planets[7]
         neptune = planets[8]
         pluto = planets[9]
+        luna = planets[301]
 
+        planet_hash = {
+                        'mercury': mercury,
+                        'venus': venus,
+                        'mars': mars,
+                        'jupiter': jupiter,
+                        'saturn': saturn,
+                        'uranus': uranus,
+                        'neptune': neptune,
+                        'pluto': pluto,
+                        'luna': luna
+                        }
         location = earth + Topos(latitude, longitude)
-
-        # for body in celestial_bodies:
-        #     astrometric = location.at(t).observe(mars)
-        #     alt, az, d = astrometric.apparent().altaz()
-
         ts = load.timescale()
         t = ts.now()
-        return 1
+
+        results = {
+                'data': {'latitude': str(latitude),
+                         'longitude': str(longitude),
+                         'time': t.utc_strftime('%Y-%m-%d %H:%M:%S'),
+                         'celestial_bodies': []
+                        }
+                }
+
+        for body in celestial_bodies:
+            planet = planet_hash[body]
+            astrometric = location.at(t).observe(planet)
+            el, az, d = astrometric.apparent().altaz()
+            ra, dec, distance = astrometric.apparent().radec()
+
+            body_output = {
+                            'name': body,
+                            'decimal_attributes': {
+                                                    'ra': str(ra._degrees),
+                                                    'dec': str(dec._degrees),
+                                                    'az': str(az._degrees),
+                                                    'el': str(el._degrees)
+                                                  },
+                            'strf_attributes': {
+                                                'ra': str(ra),
+                                                'dec': str(dec),
+                                                'az': str(az),
+                                                'el': str(el)
+                                                }
+                            }
+            results['data']['celestial_bodies'].append(body_output)
+        return results
 
 # 0 SOLAR SYSTEM BARYCENTER,
 # 1 MERCURY BARYCENTER,
