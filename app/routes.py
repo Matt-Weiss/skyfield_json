ab04d7b75ab81cea88979c52e0540edd78771873
Matt Levy <milevy1@gmail.com>
2019-05-26 16:13:58 -0600

    Adds telescope endpoint for RA and DEC start/end
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -16,3 +16,13 @@ def ephemerides():
     result = skyfield_service.SkyfieldService.ephemerides(latitude, longitude, celestial_bodies)
 
     return json.dumps(result)
+
+@app.route('/telescope_tracking')
+def telescope_tracking():
+    latitude = str(request.args.get('latitude')).replace("_", " ").upper()
+    longitude = str(request.args.get('longitude')).replace("_", " ").upper()
+    celestial_body = str(request.args.get('body'))
+
+    result = skyfield_service.SkyfieldService.telescope_tracking(latitude, longitude, celestial_body)
+
+    return json.dumps(result)
--- app/skyfield_service.py
+++ app/skyfield_service.py
@@ -1,4 +1,5 @@
 from skyfield.api import load, Topos
+import datetime
 
 class SkyfieldService():
     def ephemerides(latitude, longitude, celestial_bodies):
@@ -60,6 +61,63 @@ class SkyfieldService():
                                                 }
                             }
             results['data']['celestial_bodies'].append(body_output)
+        return results
+
+    def telescope_tracking(latitude, longitude, celestial_body):
+        planets = load('de421.bsp')
+        earth = planets[399]
+        mercury = planets[1]
+        venus = planets[2]
+        mars = planets[4]
+        jupiter = planets[5]
+        saturn = planets[6]
+        uranus = planets[7]
+        neptune = planets[8]
+        pluto = planets[9]
+        luna = planets[301]
+
+        planet_hash = {
+                        'mercury': mercury,
+                        'venus': venus,
+                        'mars': mars,
+                        'jupiter': jupiter,
+                        'saturn': saturn,
+                        'uranus': uranus,
+                        'neptune': neptune,
+                        'pluto': pluto,
+                        'luna': luna
+                        }
+        location = earth + Topos(latitude, longitude)
+        ts = load.timescale()
+
+        start_time = ts.now()
+        start_time_utc_strftime = start_time.utc_strftime('%Y-%m-%d %H:%M:%S')
+
+        end_time_utc = start_time.utc_datetime() + datetime.timedelta(0,3600)
+        end_time = ts.utc(end_time_utc)
+        end_time_utc_strftime = end_time.utc_strftime('%Y-%m-%d %H:%M:%S')
+
+        planet = planet_hash[celestial_body]
+
+        start_astrometric = location.at(start_time).observe(planet)
+        start_ra, start_dec, start_distance = start_astrometric.apparent().radec()
+
+        end_astrometric = location.at(end_time).observe(planet)
+        end_ra, end_dec, end_distance = end_astrometric.apparent().radec()
+
+        results = {
+                    'data': {'latitude': str(latitude),
+                             'longitude': str(longitude),
+                             'start_time': start_time_utc_strftime,
+                             'end_time': end_time_utc_strftime,
+                             'celestial_body': celestial_body,
+                             'start_dec': str(start_dec._degrees),
+                             'start_ra': str(start_ra._hours),
+                             'end_dec': str(end_dec._degrees),
+                             'end_ra': str(end_ra._hours)
+                            }
+                    }
+
         return results
 
 # 0 SOLAR SYSTEM BARYCENTER,
